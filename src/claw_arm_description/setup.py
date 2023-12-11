523a950d8a3878abade04b07061fb7a4cb8bf53e
Anna-Lee McLean <mcleanannalee@gmail.com>
2023-12-10 22:37:45 -0800

    Added stand-alone package for urdf and rviz config files for robot arm.

---
+++ src/claw_arm_description/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'claw_arm_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
+        (os.path.join('share', package_name, 'rviz_config'), glob(os.path.join('rviz_config', '*rviz*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='anna-lee',
+    maintainer_email='mcleanannalee@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
