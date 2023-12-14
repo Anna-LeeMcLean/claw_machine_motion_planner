7e2a099cd6f0a7f7089bbc0088058ffd9b9b187d
Anna-Lee McLean <mcleanannalee@gmail.com>
2023-12-14 01:28:32 -0800

    Updated motion_planner package to use state design pattern to make state change and creation more scalable.

--- src/motion_planner/motion_planner/motion_plan.py
+++ src/motion_planner/motion_planner/motion_plan.py
@@ -1,23 +1,14 @@
-from geometry_msgs.msg import Point
-from motion_planner.utils import Prize, State
-
-MAX_CLAW_HEIGHT_M = 1.8         # 180cm in world z axis
-STARTING_POINT_X = 0.5          # 50cm in world x axis
-STARTING_POINT_Y = 0.5          # 50cm in world y axis
-DROPOFF_POINT_X = 0.05          # 5cm in world x axis
-DROPOFF_POINT_Y = 0.05          # 5cm in world y axis
-DROPOFF_POINT_Z = 0.5           # 50cm in world z axis
+from motion_planner.utils import Prize
+from motion_planner.motion_state import MotionState, StartState
 
 
 class MotionPlan():
-    starting_point = Point(x=STARTING_POINT_X, y=STARTING_POINT_Y, z=MAX_CLAW_HEIGHT_M)
-    dropoff_point = Point(x=DROPOFF_POINT_X, y=DROPOFF_POINT_Y, z=DROPOFF_POINT_Z)
 
     def __init__(self, prize:Prize) -> None:
 
         self._total_elapsed_time = 0 
         self.prize = prize
-        self._states : list[State] = []
+        self._states : list[MotionState] = []
     
     @property
     def states(self):
@@ -27,92 +18,21 @@ class MotionPlan():
     def total_elapsed_time(self):
         return self._total_elapsed_time
 
-    def _get_prize_standoff_point(self) -> Point:
-
-        prize_standoff = Point()
-        prize_standoff.x = self.prize.centroid.x
-        prize_standoff.y = self.prize.centroid.y
-        prize_standoff.z = MAX_CLAW_HEIGHT_M
-
-        return prize_standoff
-    
-    def _get_dropoff_standoff_point(self) -> Point:
-
-        dropoff_standoff = Point()
-        dropoff_standoff.x = self.dropoff_point.x
-        dropoff_standoff.y = self.dropoff_point.y
-        dropoff_standoff.z = MAX_CLAW_HEIGHT_M
-
-        return dropoff_standoff
     
     def plan_states(self):
 
-        # 1. Starting State
-        starting_state = State(position=self.starting_point)
-        self._states.append(starting_state)
+        MotionState.prize = self.prize
 
-        # 2. Prize Standoff State (180cm above the prize)
-        prize_standoff_point = self._get_prize_standoff_point()
-        prize_standoff_state = State(position=prize_standoff_point)
-        prize_standoff_state.set_elapsed_time(starting_point=starting_state.position)
-        self._states.append(prize_standoff_state)
-
-        # 3. Open Claw
-        open_claw_state = State(position=prize_standoff_state.position)
-        open_claw_state.set_elapsed_time(starting_point=open_claw_state.position, claw_activated=True)
-        open_claw_state.set_claw_state(value=True)
-        self._states.append(open_claw_state)
-
-        # 4. Go to Prize
-        go_to_prize_state = State(position=self.prize.centroid)
-        go_to_prize_state.set_elapsed_time(starting_point=open_claw_state.position)
-        go_to_prize_state.set_claw_state(value=True)
-        self._states.append(go_to_prize_state)
-
-        # 5. Close Claw
-        close_claw_state = State(position=self.prize.centroid)
-        close_claw_state.set_elapsed_time(starting_point=go_to_prize_state.position, claw_activated=True)
-        self._states.append(close_claw_state)
-
-        # 6. Prize Standoff State (180cm above the prize)
-        prize_standoff_point = self._get_prize_standoff_point()
-        prize_standoff_state = State(position=prize_standoff_point)
-        prize_standoff_state.set_elapsed_time(starting_point=go_to_prize_state.position)
-        self._states.append(prize_standoff_state)
-
-        # 7. Dropoff Standoff State (180cm above the dropoff)
-        dropoff_standoff_point = self._get_dropoff_standoff_point()
-        dropoff_standoff_state = State(position=dropoff_standoff_point)
-        dropoff_standoff_state.set_elapsed_time(starting_point=prize_standoff_state.position)
-        self._states.append(dropoff_standoff_state)
-
-        # 8. Go to Dropoff
-        go_to_dropoff_state = State(position=self.dropoff_point)
-        go_to_dropoff_state.set_elapsed_time(starting_point=dropoff_standoff_state.position)
-        self._states.append(go_to_dropoff_state)
-
-        # 9. Open Claw
-        open_claw_state = State(position=go_to_dropoff_state.position)
-        open_claw_state.set_elapsed_time(starting_point=open_claw_state.position, claw_activated=True)
-        open_claw_state.set_claw_state(value=True)
-        self._states.append(open_claw_state)
-
-        # 10. Dropoff Standoff State (180cm above the dropoff)
-        dropoff_standoff_point = self._get_dropoff_standoff_point()
-        dropoff_standoff_state = State(position=dropoff_standoff_point)
-        dropoff_standoff_state.set_elapsed_time(starting_point=open_claw_state.position)
-        dropoff_standoff_state.set_claw_state(value=True)
-        self._states.append(dropoff_standoff_state)
-
-        # 11. Close Claw
-        close_claw_state = State(position=dropoff_standoff_state.position)
-        close_claw_state.set_elapsed_time(starting_point=close_claw_state.position, claw_activated=True)
-        self._states.append(close_claw_state)
-
-        # 12. Back to Starting Point
-        back_to_start_state = State(position=self.starting_point)
-        back_to_start_state.set_elapsed_time(starting_point=close_claw_state.position)
-        self._states.append(back_to_start_state)
+        current_state = StartState()
+        next_state = current_state.get_next_motion_state()
+        
+        while next_state != None:
+            current_state.set_position()
+            current_state.set_claw_state()
+            current_state.set_elapsed_time()
+            self._states.append(current_state)
+            next_state = current_state.get_next_motion_state()
+            current_state = next_state
 
 
         self._update_total_elapsed_time()
@@ -124,5 +44,3 @@ class MotionPlan():
             total += self._states[i].elapsed_time
 
         self._total_elapsed_time = total
-
-        
---
+++ src/motion_planner/motion_planner/motion_state.py
@@ -0,0 +1,232 @@
+from math import ceil
+from abc import ABC, abstractmethod
+
+from motion_planner.utils import Prize
+
+from geometry_msgs.msg import Point
+
+CLAW_SPEED_M_PER_S = 0.2        # 20cm/s -> 0.2m/s
+CLAW_ACTIVATION_TIME = 1        # 1 sec to open/close claw
+
+MAX_CLAW_HEIGHT_M = 1.8         # 180cm in world z axis
+STARTING_POINT_X = 0.5          # 50cm in world x axis
+STARTING_POINT_Y = 0.5          # 50cm in world y axis
+DROPOFF_POINT_X = 0.05          # 5cm in world x axis
+DROPOFF_POINT_Y = 0.05          # 5cm in world y axis
+DROPOFF_POINT_Z = 0.5           # 50cm in world z axis
+
+
+class MotionState(ABC):
+
+    prize: Prize
+
+    def __init__(self, prev_motion_state = None) -> None:
+        super().__init__()
+        self.position: Point = None
+        self.claw_state: bool = None     # true if claw is open and false otherwise
+        self.elapsed_time: int = None
+        self.previous_motion_state: MotionState = prev_motion_state
+        self.next_motion_state: MotionState = None
+
+    @abstractmethod
+    def set_elapsed_time(self):
+        pass
+
+    @abstractmethod
+    def set_claw_state(self):
+        pass
+
+    @abstractmethod
+    def set_position(self):
+        pass
+
+    @abstractmethod
+    def get_next_motion_state(self):
+        pass
+
+    def convert_to_dict(self) -> dict:
+
+        state_dict = {"position": {"x":self.position.x,
+                                   "y":self.position.y,
+                                   "z":self.position.z},
+                      "claw_state": self.claw_state,
+                      "elapsed_time": self.elapsed_time}
+
+        return state_dict
+
+
+class StartState(MotionState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+        self.next_motion_state = SetClawState(prev_motion_state=self)
+
+    def set_elapsed_time(self):
+        self.elapsed_time = 0
+
+    def set_claw_state(self):
+        self.claw_state = False
+
+    def set_position(self):
+        self.position = Point(x=STARTING_POINT_X, 
+                              y=STARTING_POINT_Y, 
+                              z=MAX_CLAW_HEIGHT_M)
+
+    def get_next_motion_state(self):
+        return self.next_motion_state
+
+
+class SetClawState(MotionState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+    def set_elapsed_time(self):
+        self.elapsed_time = CLAW_ACTIVATION_TIME
+
+    def set_claw_state(self):
+        if self.previous_motion_state.claw_state:
+            self.claw_state = False
+        else:
+            self.claw_state = True
+
+    def set_position(self):
+        self.position = Point(x=self.previous_motion_state.position.x, 
+                              y=self.previous_motion_state.position.y, 
+                              z=self.previous_motion_state.position.z)
+
+    def get_next_motion_state(self):
+        if isinstance(self.previous_motion_state, DropoffCentroidState):
+            self.next_motion_state = DropoffStandoffState(prev_motion_state=self)
+        else:
+            self.next_motion_state = PrizeStandoffState(prev_motion_state=self)
+
+        return self.next_motion_state
+
+
+class TranslationState(MotionState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+    def set_elapsed_time(self):
+        travel_distances = [abs(self.position.x - self.previous_motion_state.position.x),
+                            abs(self.position.y - self.previous_motion_state.position.y),
+                            abs(self.position.z - self.previous_motion_state.position.z)]
+
+        travel_times = [(1/CLAW_SPEED_M_PER_S) * distance for distance in travel_distances]
+
+        total = 0
+        for time in travel_times:
+            total += time
+
+        self.elapsed_time = ceil(total)    # round up to the nearest integer
+
+    def set_claw_state(self):
+        self.claw_state = self.previous_motion_state.claw_state
+
+    def set_position(self):
+        return super().set_position()
+
+    def get_next_motion_state(self):
+        return super().get_next_motion_state()
+
+
+class PrizeCentroidState(TranslationState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+    def set_elapsed_time(self):
+        return super().set_elapsed_time()
+
+    def set_claw_state(self):
+        return super().set_claw_state()
+    
+    def set_position(self):
+        self.position = Point(x=self.prize.centroid.x, 
+                              y=self.prize.centroid.y, 
+                              z=self.prize.centroid.z)
+    
+    def get_next_motion_state(self) -> MotionState:
+        
+        self.next_motion_state = SetClawState(prev_motion_state=self)
+
+        return self.next_motion_state
+
+
+class PrizeStandoffState(TranslationState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+    def set_elapsed_time(self):
+        return super().set_elapsed_time()
+
+    def set_claw_state(self):
+        return super().set_claw_state()
+    
+    def set_position(self):
+        self.position = Point(x=self.prize.centroid.x, 
+                              y=self.prize.centroid.y, 
+                              z=MAX_CLAW_HEIGHT_M)
+    
+    def get_next_motion_state(self) -> MotionState:
+
+        if self.previous_motion_state.claw_state:
+            self.next_motion_state = PrizeCentroidState(prev_motion_state=self)
+        else:
+            self.next_motion_state = DropoffStandoffState(prev_motion_state=self)
+
+        return self.next_motion_state
+
+
+class DropoffCentroidState(TranslationState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+    def set_elapsed_time(self):
+        return super().set_elapsed_time()
+
+    def set_claw_state(self):
+        return super().set_claw_state()
+    
+    def set_position(self):
+        self.position = Point(x=DROPOFF_POINT_X, 
+                              y=DROPOFF_POINT_Y, 
+                              z=DROPOFF_POINT_Z)
+    
+    def get_next_motion_state(self) -> MotionState:
+
+        self.next_motion_state = SetClawState(prev_motion_state=self)
+
+        return self.next_motion_state
+    
+
+class DropoffStandoffState(TranslationState):
+
+    def __init__(self, prev_motion_state=None) -> None:
+        super().__init__(prev_motion_state)
+
+    def set_elapsed_time(self):
+        return super().set_elapsed_time()
+
+    def set_claw_state(self):
+        return super().set_claw_state()
+    
+    def set_position(self):
+        self.position = Point(x=DROPOFF_POINT_X, 
+                              y=DROPOFF_POINT_Y, 
+                              z=MAX_CLAW_HEIGHT_M)
+    
+    def get_next_motion_state(self) -> MotionState:
+
+        if self.previous_motion_state.claw_state:
+            self.next_motion_state = None
+        else:
+            self.next_motion_state = DropoffCentroidState(prev_motion_state=self)
+
+        return self.next_motion_state
+    
